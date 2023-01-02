1adaeb9c809a30e2fb6beefa6b9133d60c9bf97d
OPaslavskiy <bankir.op@gmail.com>
2023-01-03 00:15:03 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
+import Notiflix from 'notiflix';
 
+const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+const inputForCountry = document.querySelector('#search-box');
+
+let userrRequest;
+
+inputForCountry.addEventListener(
+  'input',
+  debounce(usersSearch, DEBOUNCE_DELAY)
+);
+
+function usersSearch() {
+  userrRequest = inputForCountry.value;
+  fetchCountries(userrRequest).then(data => {
+    if (data.length > 10) {
+      Notiflix.Notify.info(
+        'Too many matches found. Please enter a more specific name.'
+      );
+    } else console.log(data);
+  });
+}
